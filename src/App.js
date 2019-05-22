2ab035fe4b10079d562c642ce39f151dd7da9dfe
Shaneil Clarke <shaneilske@yahoo.com>
2019-05-22 14:40:08 +0100

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import NavBar from './components/navbar/NavBar'
+
 function App() {
   return (
     <div>
-      Hello there
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar =()=>{
+  return(
+    <nav>
+      <header>
+        ToDo App
+      </header>
+    </nav>
+  )
+}
+
+export default NavBar
