c5845e1334173a26840242c29c282b903d5d4162
Shaneil Clarke <shaneilske@yahoo.com>
2019-05-28 19:59:48 +0100

    updated NavBar component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from 'react'
 
 import NavBar from './components/navbar/NavBar'
+import ToDo from './components/todo/ToDo';
 
 const App =()=>{
   return (
     <div>
       <NavBar />
+      <ToDo />
     </div>
   );
 }
 
-export default App;
+export default App
