e9cbc5cd81dd96609883186772a98127947a7514
Shaneil Clarke <shaneilske@yahoo.com>
2019-05-28 19:58:11 +0100

    added todo list component

---
+++ src/components/todo/ToDo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ToDo =()=>{
+  return(
+    <div>
+      <input type='checkbox'/>
+      <p>Example #1</p>
+      <input type='checkbox'/>
+      <p>Example #2</p>
+      <input type='checkbox'/>
+      <p>Example #1</p>
+    </div>
+  )
+}
+
+export default ToDo
