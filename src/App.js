9826136199e8f078b793547f07a02a20a1163b5c
Shaneil Clarke <shaneilske@yahoo.com>
2019-05-29 15:50:02 +0100

    updated components to class from functionalcomponents

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import NavBar from './components/navbar/NavBar'
 import ToDo from './components/todo/ToDo'
+import ToDoData from './components/todo/ToDoData'
 import './App.css'
 
-import ToDoData from './components/todo/ToDoData'
-
-const App =()=>{
-  const ToDoComponents = ToDoData.map((todo)=>{
+class App extends Component{
+  render(){
+    const ToDoComponents = ToDoData.map((todo)=>{
+      return(
+        <ToDo key={todo.id} items={todo}/>
+      )
+    })
     return(
-      <ToDo key={todo.id} items={todo}/>
-    )
-  })
-  return (
-    <div>
-      <NavBar />
-      <div className='todo-list'>
-        {ToDoComponents}
+      <div>
+        <NavBar />
+        <div className='todo-list'>
+          {ToDoComponents}
+        </div> 
       </div>
-      
-    </div>
-  );
+    )
+  }
 }
 
 export default App
--- src/components/todo/ToDo.js
+++ src/components/todo/ToDo.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './ToDo.css'
 
-const ToDo =(props)=>{
-  return(
-    <div className='todo_item'>
-      <input type='checkbox'/>
-      <p>{props.items.text}</p>
-    </div>
-    
-  )
+class ToDo extends Component{
+  render(){
+    return(
+      <div className='todo_item'>
+        <input type='checkbox'/>
+        <p>{this.props.items.text}</p>
+      </div>
+    )
+  }
 }
 
 export default ToDo
